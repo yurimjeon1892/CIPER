42a10c9e402ff41da1604caa9b60ae5e3ede0536
yurimjeon1892 <yurimjeon1892@gmail.com>
2023-11-23 16:24:12 +0900

    s

--- models/ciper.py
+++ models/ciper.py
@@ -13,34 +13,35 @@ from .encoder import Encoder
 from .recoder import Recoder
 from .decoder import Decoder, TwoWayDecoder
 
+
 class CIPER(nn.Module):
-    """ This is the CIPER module that performs cross-view image geo-localization """
+    """This is the CIPER module that performs cross-view image geo-localization"""
+
     def __init__(self, args):
-        """ Initializes the model.
+        """Initializes the model.
         Parameters:
             backbone: torch module of the backbone to be used. See backbone.py
             num_queries: number of object queries, ie detection slot. This is the maximal number of objects
                          DETR can detect in a single image. For COCO, we recommend 100 queries.
         """
         super().__init__()
-        
+
         self.query_net = Encoder(args, args["grd_img_size"], mode="query")
         self.reference_net = Encoder(args, args["arl_img_size"])
         self.retr_only = args["retr_only"]
         self.rng_mask = args["rng_mask"]
-        if not self.retr_only: 
+        if not self.retr_only:
             self.rot_net = Recoder(args)
             self.pose_net = TwoWayDecoder(args)
-        
+
     def forward(self, im_grd, im_arl):
-        
         emb_grd, mem_grd, qry_emb_grd = self.query_net(im_grd)
         emb_arl, mem_arl, _ = self.reference_net(im_arl)
         outputs = {
             "grd": emb_grd,
             "arl": emb_arl,
         }
-        if not self.retr_only: 
+        if not self.retr_only:
             if self.rng_mask:
                 bev_mask, rng_mask = self.rot_net(mem_grd, mem_arl)
                 mem_arl = torch.mul(bev_mask.to(mem_arl.device), mem_arl)
@@ -48,124 +49,131 @@ class CIPER(nn.Module):
                 outputs["bev_mask"] = bev_mask
             out_pos = self.pose_net(qry_emb_grd, mem_arl)
             outputs.update(out_pos)
-        
+
         return outputs
+
 
 class SetCriterion(nn.Module):
     def __init__(self, matcher, weight_dict, eos_coef, losses):
-        """ Create the criterion.
+        """Create the criterion.
         Parameters:
         """
         super().__init__()
-        
+
         self.num_classes = 1
-        
+
         self.matcher = matcher
         self.weight_dict = weight_dict
         self.eos_coef = eos_coef
-        self.losses = losses      
+        self.losses = losses
         empty_weight = torch.ones(self.num_classes + 1)
         empty_weight[-1] = self.eos_coef
         self.register_buffer("empty_weight", empty_weight)
-        
+
         self.intermediate = {}
-                
-        if "retrieval" in losses:          
+
+        if "retrieval" in losses:
             self.soft_triplet_loss = SoftTripletBiLoss().cuda()
-        
-    def loss_retrieval(self, outputs, targets, indices):     
+
+    def loss_retrieval(self, outputs, targets, indices):
         loss_ir, mean_p, mean_n = self.soft_triplet_loss(outputs["grd"], outputs["arl"])
         losses = {"retrieval": loss_ir}
         return losses
-    
+
     def loss_labels(self, outputs, targets, indices):
         """Classification loss (NLL)
         targets dicts must contain the key "labels" containing a tensor of dim [nb_target_boxes]
         """
         assert "pred_logits" in outputs
-        src_logits = outputs["pred_logits"] # bs x num_queries x 2
+        src_logits = outputs["pred_logits"]  # bs x num_queries x 2
 
         # idx = self._get_src_permutation_idx(indices)
         # target_classes_o = torch.cat([t["labels"][J] for t, (_, J) in zip(targets, indices)])
         # target_classes = torch.full(src_logits.shape[:2], self.num_classes,
         #                             dtype=torch.int64, device=src_logits.device)
         # target_classes[idx] = target_classes_o
-      
+
         # loss_ce = F.cross_entropy(src_logits.transpose(1, 2), target_classes, self.empty_weight)
         # losses = {"labels": loss_ce}
-        
+
         idx = self._get_src_permutation_idx(indices)
         target_classes = torch.zeros_like(src_logits)
         target_classes[:, :, -1] = 1.0
         target_classes[idx] = torch.tensor([1.0, 0.0]).to(src_logits.device)
-        
+
         # print("aa", torch.sum(target_classes[:, :, 0]), torch.sum(target_classes[:, :, 1]), torch.sum(target_classes, dim=2))
-        
+
         # o = torch.flatten(src_logits, 0, 1)
         # t = torch.flatten(target_classes, 0, 1)
         o, t = src_logits, target_classes
         loss_bce = F.binary_cross_entropy_with_logits(o, t.float(), self.empty_weight)
         losses = {"labels": loss_bce}
-        
+
         return losses
-        
+
     def loss_boxes(self, outputs, targets, indices):
         """Compute the losses related to the bounding boxes, the L1 regression loss and the GIoU loss
-           targets dicts must contain the key "boxes" containing a tensor of dim [nb_target_boxes, 4]
-           The target boxes are expected in format (center_x, center_y, w, h), normalized by the image size.
+        targets dicts must contain the key "boxes" containing a tensor of dim [nb_target_boxes, 4]
+        The target boxes are expected in format (center_x, center_y, w, h), normalized by the image size.
         """
         assert "pred_boxes" in outputs
         idx = self._get_src_permutation_idx(indices)
         src_boxes = outputs["pred_boxes"][idx]
-        target_boxes = torch.cat([t["boxes"][i] for t, (_, i) in zip(targets, indices)], dim=0)
+        target_boxes = torch.cat(
+            [t["boxes"][i] for t, (_, i) in zip(targets, indices)], dim=0
+        )
 
         # loss_bbox = F.l1_loss(src_boxes, target_boxes, reduction="none")
         loss_bbox = F.mse_loss(src_boxes, target_boxes.float(), reduction="none")
 
         losses = {}
-        losses["boxes"] = loss_bbox.sum() 
+        losses["boxes"] = loss_bbox.sum()
 
         return losses
-    
+
     def loss_mask(self, outputs, targets, indices):
         src_mask = outputs["rng_mask"]
         bs, w = src_mask.size(0), src_mask.size(-1)
         marg_w = int(w / 8)
-        
-        target_boxes = torch.cat([t["boxes"][i] for t, (_, i) in zip(targets, indices)], dim=0)
+
+        target_boxes = torch.cat(
+            [t["boxes"][i] for t, (_, i) in zip(targets, indices)], dim=0
+        )
         c, s = target_boxes[:, 2], target_boxes[:, 3]
         yaw = torch.atan2(s, c)
         yaw[yaw < 0] = yaw[yaw < 0] + 2 * math.pi
-        
+
         rounded_tensor = (yaw / (2 * math.pi)) * w
         rounded_tensor = torch.clamp(rounded_tensor.round(), 0, w - 1).long()
-                
+
         target_mask = torch.zeros(src_mask.size(), requires_grad=False)
         for i in range(bs):
-            y_id = rounded_tensor[i]            
+            y_id = rounded_tensor[i]
             if y_id < marg_w:
-                target_mask[i, :, :y_id+marg_w] = 1.
-                target_mask[i, :, y_id-marg_w:] = 1.
+                target_mask[i, :, : y_id + marg_w] = 1.0
+                target_mask[i, :, y_id - marg_w :] = 1.0
             elif y_id > w - marg_w:
-                target_mask[i, :, :w-y_id] = 1.
-                target_mask[i, :, y_id-marg_w:] = 1.
+                target_mask[i, :, : w - y_id] = 1.0
+                target_mask[i, :, y_id - marg_w :] = 1.0
             else:
-                target_mask[i, :, y_id-marg_w:y_id+marg_w] = 1.
-        
+                target_mask[i, :, y_id - marg_w : y_id + marg_w] = 1.0
+
         loss_mask = F.binary_cross_entropy(src_mask, target_mask)
-        
+
         losses = {}
         losses["mask"] = loss_mask
-        
+
         self.intermediate["target_mask"] = target_mask
         return losses
-        
+
     def _get_src_permutation_idx(self, indices):
         # permute predictions following indices
-        batch_idx = torch.cat([torch.full_like(src, i) for i, (src, _) in enumerate(indices)])
+        batch_idx = torch.cat(
+            [torch.full_like(src, i) for i, (src, _) in enumerate(indices)]
+        )
         src_idx = torch.cat([src for (src, _) in indices])
         return batch_idx, src_idx
-        
+
     def get_loss(self, loss, outputs, targets, indices):
         loss_map = {
             "retrieval": self.loss_retrieval,
@@ -177,76 +185,84 @@ class SetCriterion(nn.Module):
         return loss_map[loss](outputs, targets, indices)
 
     def forward(self, outputs, targets):
-        """ This performs the loss computation.
+        """This performs the loss computation.
         Parameters:
              outputs: dict of tensors, see the output specification of the model for the format
              targets: list of dicts, such that len(targets) == batch_size.
                       The expected keys in each dict depends on the losses applied, see each loss" doc
-        """  
-        if "labels" in self.losses:      
+        """
+        if "labels" in self.losses:
             indices = self.matcher(outputs, targets)
         else:
             indices = None
-        
+
         # Compute all the requested losses
         losses = {}
         for loss in self.losses:
             losses.update(self.get_loss(loss, outputs, targets, indices))
-            
+
         for k in losses.keys():
             losses[k] = losses[k] * self.weight_dict[k]
 
-        return losses    
-    
+        return losses
+
+
 class PostProcess(nn.Module):
-    """ This module converts the model's output into the format expected by the coco api"""
+    """This module converts the model's output into the format expected by the coco api"""
+
     @torch.no_grad()
     def forward(self, outputs, targets):
-        """ Perform the computation
+        """Perform the computation
         Parameters:
             outputs: raw outputs of the model
             target_sizes: tensor of dimension [batch_size x 2] containing the size of each images of the batch
                           For evaluation, this must be the original image size (before any data augmentation)
                           For visualization, this should be the image size after data augment, but before padding
         """
-        out_logits, out_bbox = outputs["pred_logits"], outputs["pred_boxes"] # bs x num_quries x 4
-        
+        out_logits, out_bbox = (
+            outputs["pred_logits"],
+            outputs["pred_boxes"],
+        )  # bs x num_quries x 4
+
         assert len(out_logits) == len(targets)
-        
+
         # prob = F.softmax(out_logits, -1)
         # scores, labels = prob[..., :-1].max(-1)
-        
+
         prob = torch.sigmoid(out_logits)
         scores = prob[..., :-1]
-        
-        x_c, y_c, c, s = out_bbox.unbind(-1) # bs x num_quries
-        yaw = torch.atan2(s, c)       
-        
-        xs, ys = [], []        
+
+        x_c, y_c, c, s = out_bbox.unbind(-1)  # bs x num_quries
+        yaw = torch.atan2(s, c)
+
+        xs, ys = [], []
         for b in range(len(out_logits)):
             arl_img_size = targets[b]["orig_size"]
             meter_per_pixel = targets[b]["meter_per_pixel"][0]
             x = x_c[b] * arl_img_size[0] * meter_per_pixel
-            y = y_c[b] * arl_img_size[1] * meter_per_pixel           
+            y = y_c[b] * arl_img_size[1] * meter_per_pixel
             xs.append(x)
             ys.append(y)
         xs = torch.stack(xs, 0)
-        ys = torch.stack(ys, 0)  
-         
+        ys = torch.stack(ys, 0)
+
         boxes = torch.stack([xs, ys, yaw], dim=-1)
-        
+
         rng_mask, bev_mask = outputs["rng_mask"], outputs["bev_mask"]
 
         # results = [{"scores": s, "labels": l, "boxes": b} for s, l, b in zip(scores, labels, boxes)]
-        # results = [{"scores": s, "boxes": b} for s, b in zip(scores, boxes)]  
-        results = [{"scores": s, "boxes": b, "rng_mask": rm, "bev_mask": bm} for s, b, rm, bm in zip(scores, boxes, rng_mask, bev_mask)]
+        # results = [{"scores": s, "boxes": b} for s, b in zip(scores, boxes)]
+        results = [
+            {"scores": s, "boxes": b, "rng_mask": rm, "bev_mask": bm}
+            for s, b, rm, bm in zip(scores, boxes, rng_mask, bev_mask)
+        ]
         return results
-    
+
+
 def build(args):
-    
     model = CIPER(args)
-        
-    # build criterion    
+
+    # build criterion
     if args["retr_only"]:
         matcher = None
         weight_dict = {"retrieval": 1}
@@ -254,15 +270,22 @@ def build(args):
         losses = ["retrieval"]
     else:
         matcher = build_matcher(args)
-        weight_dict = {"retrieval": 1, "labels": args["label_loss_coef"], "boxes": args["bbox_loss_coef"], "mask": 1.}
+        weight_dict = {
+            "retrieval": 1,
+            "labels": args["label_loss_coef"],
+            "boxes": args["bbox_loss_coef"],
+            "mask": 1.0,
+        }
         eos_coef = args["eos_coef"]
-        losses = ["retrieval", "labels", "boxes", "mask"]   
-    criterion = SetCriterion(matcher=matcher, weight_dict=weight_dict, eos_coef=eos_coef, losses=losses)
-    
-    # build post processor           
+        losses = ["retrieval", "labels", "boxes", "mask"]
+    criterion = SetCriterion(
+        matcher=matcher, weight_dict=weight_dict, eos_coef=eos_coef, losses=losses
+    )
+
+    # build post processor
     if args["retr_only"]:
         postprocessors = None
     else:
         postprocessors = PostProcess()
-    
+
     return model.to(args["device"]), criterion.to(args["device"]), postprocessors
--- models/decoder.py
+++ models/decoder.py
@@ -3,73 +3,95 @@ from torch import nn
 
 from .common import MLP
 from .transformer import TransformerDecoder, TransformerDecoderLayer
-   
+
+
 class Decoder(nn.Module):
     def __init__(self, args):
-        """ Initializes the model.
-        """
+        """Initializes the model."""
         super().__init__()
-        
-        decoder_layer = TransformerDecoderLayer(d_model=args["dim_embed"], 
-                                                nhead=args["num_heads"], 
-                                                dim_feedforward=args["dim_feedforward"],
-                                                dropout=args["dropout"], 
-                                                activation="relu", 
-                                                normalize_before=args["pre_norm"])
+
+        decoder_layer = TransformerDecoderLayer(
+            d_model=args["dim_embed"],
+            nhead=args["num_heads"],
+            dim_feedforward=args["dim_feedforward"],
+            dropout=args["dropout"],
+            activation="relu",
+            normalize_before=args["pre_norm"],
+        )
         decoder_norm = nn.LayerNorm(args["dim_embed"])
-        self.decoder = TransformerDecoder(decoder_layer, args["num_dec_layers"], decoder_norm,
-                                          return_intermediate=False)      
-                
-        self.class_embed = nn.Linear(args["dim_embed"], 2)      
-        self.bbox_embed = MLP(args["dim_embed"], args["dim_embed"], output_dim=4, num_layers=3)           
-                
+        self.decoder = TransformerDecoder(
+            decoder_layer,
+            args["num_dec_layers"],
+            decoder_norm,
+            return_intermediate=False,
+        )
+
+        self.class_embed = nn.Linear(args["dim_embed"], 2)
+        self.bbox_embed = MLP(
+            args["dim_embed"], args["dim_embed"], output_dim=4, num_layers=3
+        )
+
         self.num_queries = args["num_queries"]
-        self.query_embed = nn.Embedding(self.num_queries, args["dim_embed"])    
-        
-    def forward(self, x_grd, x_arl):   
+        self.query_embed = nn.Embedding(self.num_queries, args["dim_embed"])
+
+    def forward(self, x_grd, x_arl):
         """
-            x_grd: bs x dim_embed
-            x_arl: bs x num_patches x dim_embed
-        """        
-        x_grd = torch.repeat_interleave(x_grd.unsqueeze(0), self.num_queries, dim=0) # num_queries x bs x dim_embed          
-        query_pos = torch.repeat_interleave(self.query_embed.weight.unsqueeze(1), x_grd.shape[1], dim=1) # num_queries x bs x dim_embed
-              
-        x_arl = x_arl.permute(1, 0, 2) # num_patches x bs x dim_embed
+        x_grd: bs x dim_embed
+        x_arl: bs x num_patches x dim_embed
+        """
+        x_grd = torch.repeat_interleave(
+            x_grd.unsqueeze(0), self.num_queries, dim=0
+        )  # num_queries x bs x dim_embed
+        query_pos = torch.repeat_interleave(
+            self.query_embed.weight.unsqueeze(1), x_grd.shape[1], dim=1
+        )  # num_queries x bs x dim_embed
+
+        x_arl = x_arl.permute(1, 0, 2)  # num_patches x bs x dim_embed
         # print("input", x_grd.size(), query_pos.size(), x_arl.size(), pos.size())
-        
-        dst = self.decoder(tgt=x_grd, memory=x_arl, 
-                           query_pos=query_pos) 
-        dst = dst.transpose(1, 2) # 1 x bs x num_queries x dim_embed
+
+        dst = self.decoder(tgt=x_grd, memory=x_arl, query_pos=query_pos)
+        dst = dst.transpose(1, 2)  # 1 x bs x num_queries x dim_embed
         # print("dst: ", dst.size())
-        
-        outputs_class = self.class_embed(dst) # 1 x bs x num_queries x 2
-        outputs_coord = self.bbox_embed(dst) # 1 x bs x num_queries x 4   
-        # print("out: ", outputs_class.size(), outputs_coord.size())     
-        
-        out = {'pred_logits': outputs_class[-1], 'pred_boxes': outputs_coord[-1]} # [-1]: last decoder layer output
+
+        outputs_class = self.class_embed(dst)  # 1 x bs x num_queries x 2
+        outputs_coord = self.bbox_embed(dst)  # 1 x bs x num_queries x 4
+        # print("out: ", outputs_class.size(), outputs_coord.size())
+
+        out = {
+            "pred_logits": outputs_class[-1],
+            "pred_boxes": outputs_coord[-1],
+        }  # [-1]: last decoder layer output
         return out
+
 
 # From https://github.com/facebookresearch/segment-anything/blob/HEAD/segment_anything/modeling/mask_decoder.py
 from .prompt_encoder import PositionEmbeddingRandom
 from .twowaytransformer import TwoWayTransformer
 
+
 class TwoWayDecoder(nn.Module):
     def __init__(self, args):
         super().__init__()
-        
+
         self.pe_layer = PositionEmbeddingRandom(args["dim_embed"] // 2)
-        
-        self.transformer = TwoWayTransformer(depth=2,
-                                             embedding_dim=args["dim_embed"],
-											 mlp_dim=2048,
-											 num_heads=8,
-											)
-        
-        self.class_embed = nn.Linear(args["dim_embed"], 2)      
-        self.bbox_embed = MLP(args["dim_embed"], args["dim_embed"], output_dim=4, num_layers=3) 
-        
+
+        self.transformer = TwoWayTransformer(
+            depth=2,
+            embedding_dim=args["dim_embed"],
+            mlp_dim=2048,
+            num_heads=8,
+        )
+
+        self.class_embed = nn.Linear(args["dim_embed"], 2)
+        self.bbox_embed = MLP(
+            args["dim_embed"], args["dim_embed"], output_dim=4, num_layers=3
+        )
+
         self.num_queries = args["num_queries"]
-        self.image_embedding_size = (int(args["arl_img_size"][0] / args["patch_size"]), int(args["arl_img_size"][1] / args["patch_size"]))
+        self.image_embedding_size = (
+            int(args["arl_img_size"][0] / args["patch_size"]),
+            int(args["arl_img_size"][1] / args["patch_size"]),
+        )
 
     def forward(
         self,
@@ -88,21 +110,32 @@ class TwoWayDecoder(nn.Module):
           torch.Tensor: batched predictions of mask quality
         """
         src = key_img_embedding
-        
-        key_img_pe = self.pe_layer(self.image_embedding_size).unsqueeze(0) # 1 x dim_embed x h x w
-        key_img_pe = key_img_pe.flatten(2).permute(0, 2, 1) # 1 x num_patches x dim_embed 
-        pos_src = torch.repeat_interleave(key_img_pe, query_img_embedding.shape[0], dim=0) # bs x num_patches x dim_embed 
-                
-        query_img_embedding = torch.repeat_interleave(query_img_embedding.unsqueeze(1), self.num_queries, dim=1)
+
+        key_img_pe = self.pe_layer(self.image_embedding_size).unsqueeze(
+            0
+        )  # 1 x dim_embed x h x w
+        key_img_pe = key_img_pe.flatten(2).permute(
+            0, 2, 1
+        )  # 1 x num_patches x dim_embed
+        pos_src = torch.repeat_interleave(
+            key_img_pe, query_img_embedding.shape[0], dim=0
+        )  # bs x num_patches x dim_embed
+
+        query_img_embedding = torch.repeat_interleave(
+            query_img_embedding.unsqueeze(1), self.num_queries, dim=1
+        )
         tokens = query_img_embedding
-        
+
         # Run the transformer
         hs, src = self.transformer(src, pos_src, tokens)
         dst = hs.unsqueeze(0)
-        
-        outputs_class = self.class_embed(dst) # 1 x bs x num_queries x 2
-        outputs_coord = self.bbox_embed(dst) # 1 x bs x num_queries x 4   
-        # print("out: ", outputs_class.size(), outputs_coord.size())     
-        
-        out = {'pred_logits': outputs_class[-1], 'pred_boxes': outputs_coord[-1]} # [-1]: last decoder layer output
+
+        outputs_class = self.class_embed(dst)  # 1 x bs x num_queries x 2
+        outputs_coord = self.bbox_embed(dst)  # 1 x bs x num_queries x 4
+        # print("out: ", outputs_class.size(), outputs_coord.size())
+
+        out = {
+            "pred_logits": outputs_class[-1],
+            "pred_boxes": outputs_coord[-1],
+        }  # [-1]: last decoder layer output
         return out
